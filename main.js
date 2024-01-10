04ef5b9888b5494d0c1b2914238cc11dd68518de
Kylin <1159469891@qq.com>
2024-01-10 11:22:44 +0800

    feat: 修改action！

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 const log = require('./utils/log')
+const Action = require('./actions')
 ;(async () => {
   try {
     // 获取输入
@@ -14,24 +15,15 @@ const log = require('./utils/log')
     log.info(`branch: ${branch}`)
     log.info(`github: ${github}`)
     const { context } = github.context
-    const { owner, repo } = context.repo
+    const owner = context.repo.owner
+    const repo = context.repo.repo
     log.info(`owner: ${owner}`)
     log.info(`repo: ${repo}`)
     const payload = { token, JueJinId, commit_message, branch, owner, repo }
+
+    await Action(payload)
   } catch (error) {
     log.error(error)
     core.setFailed(error.message)
   }
 })()
-
-// const getJueJinInfo = require('./crawler/juejin')
-// const renderJueJinCard = require('./render/juejin')
-
-// async function renderJueJin(id) {
-//   const data = await getJueJinInfo(id)
-//   renderJueJinCard(data)
-// }
-
-// const JueJinId = core.getInput('JueJinId')
-
-// renderJueJin(JueJinId || '4344859055106215')
