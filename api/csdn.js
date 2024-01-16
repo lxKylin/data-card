a6e5d696a527184b05f7ed2dbda89acb23f24eb9
Kylin <1159469891@qq.com>
2024-01-16 20:38:43 +0800

    feat: 完成CSDN中文卡片

--- api/csdn.js
+++ api/csdn.js
@@ -14,31 +14,39 @@ async function getCSDNInfo(name = 'weixin_46926182') {
     // 示例：使用选择器获取标题
     username: $('title').text().split('-')[0],
     achievement: $('.aside-common-box-achievement div').text(),
-    achievement2: $('.aside-common-box-achievement div span').text(),
+    achievement3: $('.aside-common-box-achievement div')
+      .text()
+      .split('得')
+      .splice(1)
+      .map((item) => item.split('次')[0]),
     test3: $('.user-profile-head-info-r-c')
       .text()
       .split('    ')[1]
       .split('  ')
-      .filter((item) => item !== ''),
-    name: $('.user-profile-head-name').text()
+      .filter((item) => item !== '')
 
     // 示例：使用选择器获取其他数据
     // otherData: $('其他选择器').text(),
   }
 
-  jsonData.views = Number(jsonData.test3[0].split(' ')[1].split(',').join(''))
+  jsonData.views = Number(jsonData.test3[0].split(' ')[1].split(',').join('')) // 总访问量
   jsonData.article_count = Number(
     jsonData.test3[1].split(' ')[0].split(',').join('')
-  )
-  jsonData.ranking = Number(jsonData.test3[2].split(' ')[0].split(',').join(''))
+  ) // 原创文章数
+  jsonData.ranking = Number(jsonData.test3[2].split(' ')[0].split(',').join('')) // 排名
   jsonData.follow_count = Number(
     jsonData.test3[3].split(' ')[0].split(',').join('')
-  )
-
+  ) // 粉丝数
+  jsonData.likes = Number(jsonData.achievement3[0]) // 点赞数
+  jsonData.discuss = Number(jsonData.achievement3[1]) // 评论数
+  jsonData.collect = Number(jsonData.achievement3[2]) // 收藏数
+  jsonData.share = Number(jsonData.achievement3[3]) // 分享数
   // 打印转化后的JSON数据
   console.log(jsonData, 'jsonData')
 
   return jsonData
 }
+
+getCSDNInfo()
 
 module.exports = getCSDNInfo
--- render/csdn.js
+++ render/csdn.js
@@ -10,7 +10,18 @@ function renderCSDNCard(data, lang = 'zh-CN') {
   console.log(data, 'csdn1')
   data = formatData(data)
   console.log(data, 'csdn2')
-  let { username, article_count, ranking, follow_count, views, theme } = data
+  let {
+    username,
+    article_count,
+    ranking,
+    follow_count,
+    views,
+    likes,
+    discuss,
+    collect,
+    share,
+    theme
+  } = data
   let items = []
   switch (lang) {
     case 'zh-CN':
@@ -21,12 +32,22 @@ function renderCSDNCard(data, lang = 'zh-CN') {
         constructItem(97, 44, `${username}的 CSDN 数据`, 'title', 18),
         constructItem(63, 83, `原创`, 'label', 13.5),
         constructItem(63, 120, `粉丝`, 'label', 13.5),
-        constructItem(63, 157, `总访问量`, 'label', 13.5),
+        constructItem(220, 120, `总访问量`, 'label', 13.5),
         constructItem(220, 83, `排名`, 'label', 13.5),
+        constructItem(63, 163, `成就`, 'label', 13.5),
+        constructItem(124, 153, `被点赞`, 'label', 13.5),
+        constructItem(230, 153, `被评论`, 'label', 13.5),
+        constructItem(124, 173, `被收藏`, 'label', 13.5),
+        constructItem(230, 173, `被分享`, 'label', 13.5),
         constructItem(134, 83, `${formatNumber(article_count)}`, 'value', 15),
         constructItem(134, 120, `${formatNumber(follow_count)}`, 'value', 15),
-        constructItem(295, 83, `${formatNumber(views)}`, 'value', 15),
-        constructItem(134, 157, `${formatNumber(ranking)}`, 'value', 15)
+        constructItem(295, 120, `${formatNumber(views)}`, 'value', 15),
+        constructItem(295, 83, `${formatNumber(ranking)}`, 'value', 15),
+        constructItem(295, 83, `${formatNumber(ranking)}`, 'value', 15),
+        constructItem(185, 153, `${formatNumber(likes)}`, 'value', 15),
+        constructItem(295, 153, `${formatNumber(discuss)}`, 'value', 15),
+        constructItem(185, 173, `${formatNumber(collect)}`, 'value', 15),
+        constructItem(295, 173, `${formatNumber(share)}`, 'value', 15)
       ]
       break
     default:
